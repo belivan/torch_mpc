7bcbc18f3d8a01f62fbeaf32079aad132140b11e
belivan <anton.yanovich@hotmail.com>
2024-04-18 16:09:26 -0400

    generic

--- src/torch_mpc/cost_functions/generic_cost_function.h
+++ src/torch_mpc/cost_functions/generic_cost_function.h
@@ -2,37 +2,84 @@
 #define GENERIC_COST_FUNCTION_IS_INCLUDED
 
 #include <torch/torch.h>
-#include "base.h"
+#include <memory>
+#include <vector>
+#include <iostream>
+#include <string>
+#include <unordered_map>
+#include <functional>
 
-class GenericCostFunction : public CostTerm // unsure if we also need to make the termfunctions?
-{
+class CostFunction {
+/*
+High level cost-term aggregator that all MPC should be using to get the costs of trajectories
+
+In order to handle constraints properly, all cost terms must produce a feasible flag, in
+    addition to a cost. We can then check if all trajs are infeasible and do something about it.
+
+Note that this feasible matrix is meant to be equivalent to allowing cost terms to prune out trajectories,
+but maintaining the batch shape.
+*/
 private:
-    std::vector<std::pair<float, TermFunctions>> cost_terms_;
-    std::vector<torch::Tensor*> data_;
-    torch::Device device_;
+    std::vector<std::shared_ptr<CostTerm>> cost_terms;
+    std::vector<double> cost_weights;
+    std::unordered_map<std::string, torch::Tensor> data;
+    torch::Device device;
 
 public:
-    bool canComputeCost() const
-    {
-        for (auto &val : data_)
-        {
-            if(!val) // checking if nullptr?
-            {
-                return false;
-            }
+    CostFunction(const std::vector<std::pair<double, 
+                std::shared_ptr<CostTerm>>>& terms, 
+                const torch::Device& device = torch::kCPU)
+    : device(device) {
+        /*
+         Args:
+            cost_terms: a list of (weight, term) tuples that comprise the cost function
+            data_timeout: time after which 
+        */
+        for (const auto& term : terms) {
+            cost_weights.push_back(term.first);
+            cost_terms.push_back(term.second);
         }
-        return true;
+        for (const auto& key : get_data_keys()) { // double check double key function
+            data[key] = torch::Tensor();  // Initialize with empty tensors
+        }
     }
 
-    CostFunction to(torch::Device device) {
-        device_ = device;
-        for (auto &term : cost_terms_)
-        {
-            term.second->to(device);
+    std::pair<torch::Tensor, torch::Tensor> cost(const torch::Tensor& states, const torch::Tensor& actions) {
+        auto costs = torch::zeros({states.size(0), states.size(1)}, states.options());
+        auto feasible = torch::ones({states.size(0), states.size(1)}, torch::kBool).to(device);
+
+        for (size_t i = 0; i < cost_terms.size(); ++i) {
+            auto [new_cost, new_feasible] = cost_terms[i]->cost(states, actions, feasible, data);
+            costs += cost_weights[i] * new_cost;
+            feasible = feasible.logical_and(new_feasible);
         }
-        return *this;
+
+        return {costs, feasible};
     }
 
+    std::vector<std::string> get_data_keys() const {
+        std::set<std::string> keys;
+        for (const auto& term : cost_terms) {
+            auto term_keys = term->get_data_keys();
+            keys.insert(term_keys.begin(), term_keys.end());
+        }
+        return std::vector<std::string>(keys.begin(), keys.end());
+    }
+
+    void to(const std::string& device_type) {
+        device = torch::Device(device_type);
+        for (auto& term : cost_terms) {
+            term->to(device);  // Assuming CostTerm also supports a to(device) method
+        }
+    }
+
+    friend std::ostream& operator<<(std::ostream& os, const CostFunction& cf) {
+        os << "Cost Function containing:\n";
+        for (size_t i = 0; i < cf.cost_terms.size(); ++i) {
+            os << "\t" << cf.cost_weights[i] << ":\t" << *cf.cost_terms[i] << "\n";
+        }
+        return os;
+    }
 };
 
-#endif
+#endif // COST_FUNCTION_H
