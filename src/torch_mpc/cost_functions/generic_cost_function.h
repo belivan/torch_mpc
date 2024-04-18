28ceb7b938836a5041188198d9170f3d86934620
Pearl Franz <pfranz@andrew.cmu.edu>
2024-04-17 21:08:25 -0400

    starting generic cost function

---
+++ src/torch_mpc/cost_functions/generic_cost_function.h
@@ -0,0 +1,41 @@
+#ifndef GENERIC_COST_FUNCTION_IS_INCLUDED
+#define GENERIC_COST_FUNCTION_IS_INCLUDED
+
+#include <torch/torch.h>
+
+class GENERIC_COST_FUNCTION_IS_INCLUDED // unsure if we also need to make the termfunctions?
+{
+private:
+    std::vector<std::pair<float, TermFunctions>> cost_terms_;
+    std::vector<torch::Tensor*> data_;
+    torch::Device device_;
+    
+
+public:
+
+
+
+    bool canComputeCost() const
+    {
+        for (auto &val : data_)
+        {
+            if(!val) // checking if nullptr?
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    CostFunction to(torch::Device device) {
+        device_ = device;
+        for (auto &term : cost_terms_)
+        {
+            term.second->to(device);
+        }
+        return *this;
+    }
+
+};
+
+#endif
