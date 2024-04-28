bf96c7e7475a38b12f2daabc8f162b48f1ce24fb
belivan <anton.yanovich@hotmail.com>
2024-04-28 19:57:20 -0400

    getting closer to graphing

--- src/torch_mpc/algos/viz_master.py
+++ src/torch_mpc/algos/viz_master.py
@@ -2,6 +2,7 @@ import torch
 import numpy as np
 import os
 import matplotlib.pyplot as plt
+
 import yaml
 
 # Set current directory to this file's directory
@@ -45,43 +46,36 @@ device = config['common']['device']
 
 num_trajectories = X.shape[0]
 
-terminal_traj = 0
-terminal_traj_last = 0
+current_traj = 0
+# prev_traj = 0
 
-while terminal_traj < num_trajectories - 1:
+while current_traj < num_trajectories - 1:
     print("GRAPHING")
     fig, axs = plt.subplots(1, 3, figsize=(18, 6))
-    # for i in range(terminal_traj_last, terminal_traj):
-        if i == terminal_traj_last:
-            axs[0].plot(X[i, :, 0], X[i, :, 1], 'b-', label='Trajectory', alpha=0.1)
 
-            axs[1].plot(U[i, :, 0], 'r-', label='Throttle')
-            axs[1].plot(U[i, :, 1], 'b-', label='Steer')
+    axs[0].plot(X[current_traj, :, 0], X[current_traj, :, 1], 'b-', label='Trajectory', alpha=0.1)
 
-            # axs[0].scatter(goals[i, 0, 0, 0], goals[i, 0, 0, 1], c='g', label='Goal 1')
-            # axs[0].scatter(goals[i, 1, 0, 0], goals[i, 1, 0, 1], c='r', label='Goal 2')
-        if i > terminal_traj_last:
-            if (goals[i-1, 0, 0, 0] != goals[i, 0, 0, 0] and goals[i-1, 0, 0, 1] != goals[i, 0, 0, 1]):
-                terminal_traj = i
-                print("FOUND TERMINAL TRAJ at Goal 1")
-                print("Goal 1: ", goals[i, 0, 0, 0], goals[i, 0, 0, 1])
-                print("Sampled Traj: ", i)
-                break
-            elif (goals[i-1, 1, 0, 0] != goals[i, 1, 0, 0] and goals[i-1, 1, 1, 1] != goals[i, 1, 0, 1]):
-                terminal_traj = i
-                print("FOUND TERMINAL TRAJ at Goal 2")
-                print("Goal 2: ", goals[i, 1, 0, 0], goals[i, 1, 0, 1])
-                print("Sampled Traj: ", i)
-                break
+    axs[1].plot(U[current_traj, :, 0], 'r-', label='Throttle')
+    axs[1].plot(U[current_traj, :, 1], 'b-', label='Steer')
 
-        # if i % 50 == 0:
-        axs[0].plot(X[i, :, 0], X[i, :, 1], 'b-', alpha=0.1)
-        axs[1].plot(U[i, :, 0], 'r-')
-        axs[1].plot(U[i, :, 1], 'b-')
+    # axs[0].scatter(goals[i, 0, 0, 0], goals[i, 0, 0, 1], c='g', label='Goal 1')
+    # axs[0].scatter(goals[i, 1, 0, 0], goals[i, 1, 0, 1], c='r', label='Goal 2')
 
-        terminal_traj = i
+    dx_total = (goals[current_traj, 0, 0, 0] - X[current_traj, 0, 0])
+    dy_total = (goals[current_traj, 0, 0, 1] - X[current_traj, 0, 1])
 
-    axs[0].plot(X_real[terminal_traj-10:terminal_traj, 0], X_real[terminal_traj-10:terminal_traj, 1], 'g', label='GT')
+    dx = dx_total / np.sqrt(dx_total**2 + dy_total**2)
+    dy = dy_total / np.sqrt(dx_total**2 + dy_total**2)
+
+    axs[0].arrow(X[current_traj, 0, 0], X[current_traj, 0, 1], dx, dy,
+          shape='full', lw=0.2, length_includes_head=True, head_width=0.02, color='black', label='Goal 1')
+
+    if current_traj < 50:
+        index = 0
+    else:
+        index = current_traj - 50
+
+    axs[0].plot(X_real[index:current_traj, 0], X_real[index:current_traj, 1], 'g', label='GT')
     axs[0].set_xlabel("X Position")
     axs[0].set_ylabel("Y Position")
     axs[0].legend()
@@ -95,24 +89,23 @@ while terminal_traj < num_trajectories - 1:
     colors = 'rgbcmyk'
     for xi, name in enumerate(['X', 'Y', 'Th']):
         c = colors[xi % len(colors)]
-        for i in range(terminal_traj-10, terminal_traj):
-            if i == 0:
-                axs[2].plot(np.arange(i, i+length, 1), X[i, :, xi], c=c, label=name)
-                axs[2].plot(X_real[:, xi], label=name + ' GT', alpha=0.1)
-            if i % 50 == 0:
-                axs[2].plot(np.arange(i, i+length, 1), X[i, :, xi], c=c)
-            axs[2].plot(X_real[:, xi], alpha=0.1)
+        axs[2].plot(np.arange(current_traj, current_traj + length, 1), X[current_traj, :, xi], c=c, label=name)
+        axs[2].plot(X_real[index:current_traj, xi], label=name + ' GT', alpha=0.1)
     axs[2].set_xlabel("Time Step")
     axs[2].set_ylabel("State Value")
     axs[2].legend()
     # plt.title("Trajectories and Ground Truth States")
 
     # plt.savefig('algos_data/visualize_2000_comapre_ALL_no_goals_H50_spaced_control.png')
+    manager = plt.get_current_fig_manager()
+    manager.window.attributes('-fullscreen', True)
+
     plt.show(block=False)
-    plt.pause(0.2)
+    # plt.show()
+    plt.pause(0.07)
     plt.clf()
 
     print("DONE")
 
-    terminal_traj_last = terminal_traj
-    print("Terminal Traj: ", terminal_traj)
+    current_traj += 10
+    print("Current Trajectory", current_traj)
